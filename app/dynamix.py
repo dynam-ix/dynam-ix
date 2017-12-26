8107e73b1141526c9db3adae5ebad3ba548a3f41
Pedro de Botelho Marcos <pbmarcos@inf.ufrgs.br>
2017-12-26 15:35:31 -0200

    sockets

--- app/dynamix.py
+++ app/dynamix.py
@@ -1,10 +1,12 @@
 import sys
+import os
+import socket
 import threading
 import subprocess
 
 def cli():
     while True:
-        action = raw_input("Please type your command: ")
+        action = raw_input("Dynam-IX: ")
         if len(action) > 0:
             if "register" in action: #Register 'ID' 'asn' 'address' 'service' 'custRep' 'provRep'
                 x = subprocess.check_output('node register.js '+action, shell=True)
@@ -28,46 +30,75 @@ def cli():
             elif "updateAddress" in action: #updateAddress 'ID' 'newAddress'
                 x = subprocess.check_output('node update.js '+action, shell=True)
                 print x
-            elif "quit" in action:
-                return
+            elif "propose" in action: 
+                S = action.split(" ")[1]
+                address = S.split(":")[0]
+                port = int(S.split(":")[1])
+                clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                clientsocket.connect((address, port))
+                msg = 'proposal:5000'
+                clientsocket.send(msg)
+                clientsocket.close()
+            elif "quit" or "exit" in action:
+                 os._exit(1)
             else:
                 print "Invalid command\n"
 
     return
 
 def sendOffer():
-      reputation = verifyReputation(m.ASN):
-      if reputation >= threshold:
-        offer = checkPolicy(P, reputation) #can be based on the reputation
-        if offer not "NULL"
-            sendOffer()  
+    print "\nI will check if I can send an offer\n"
+#      reputation = verifyReputation(m.ASN):
+#      if reputation >= threshold:
+#        offer = checkPolicy(P, reputation) #can be based on the reputation
+#        if offer not "NULL"
+#            sendOffer()  
+    return 
 
 def establishAgreement():
-      if checkValidity(m):
-            createSmartContract()
-            publishSmartContract()
-            updateNetworkConfiguration()
-     else:
-            sendMessage("This offer is not valid anymore!")
+    print "\nI will check if the proposal is still valid\n"
+ #     if checkValidity(m):
+ #           createSmartContract()
+ #           publishSmartContract()
+ #           updateNetworkConfiguration()
+ #    else:
+ #           sendMessage("This offer is not valid anymore!")
+    return
 
 def collectOffer():
-    if checkValidity(m):
-          storeOffer(m)
-    if numOffers == X:
+    print "\nCollecting offer\n"
+#    if checkValidity(m):
+#          storeOffer(m)
+#    if numOffers == X:
         #select and send proposal
+    return
 
-def messages():
+def processMessages():
+
+    messageThreads = []
+
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.bind(('localhost', int(sys.argv[1]))) #load from config
+    serversocket.listen(5) # become a server socket, maximum 5 connections
 
     while True:
-        #recvMessage    
-        if msg.type == "query":
-            #create thread
-        elif msg.type == "proposal":
-            #create thread
-        elif msg.type == "offer":
-            #create thread
-        else:
-            print "Invalid message\n"
+        connection, address = serversocket.accept()
+        msg = connection.recv(1024)
+        if len(msg) > 0:
+            if msg == "query":
+                t = threading.Thread(target=sendOffer)
+                messageThreads.append(t)
+                t.start()
+            elif "proposal" in msg:
+                t = threading.Thread(target=establishAgreement)
+                messageThreads.append(t)
+                t.start()
+            elif msg == "offer":
+                t = threading.Thread(target=collectOffer)
+                messageThreads.append(t)
+                t.start()
+            else:
+                print "Invalid message\n"
 
     return
 
@@ -78,6 +109,6 @@ if __name__ == "__main__":
     t = threading.Thread(target=cli)
     threads.append(t)
     t.start()
-    t = threading.Thread(target=messages)
+    t = threading.Thread(target=processMessages)
     threads.append(t)
     t.start()
