9dc04cb4aae3b0bc0d37fec0dbfa321ba579005e
Pedro de Botelho Marcos <pbmarcos@inf.ufrgs.br>
2018-01-03 12:17:02 -0200

    complex-network

--- app/startFabric.sh
+++ app/startFabric.sh
@@ -15,16 +15,20 @@ export MSYS_NO_PATHCONV=1
 starttime=$(date +%s)
 
 # launch network; create channel and join peer to channel
-cd ../network
+cd ../test
 ./start.sh
 
 # Now launch the CLI container in order to install, instantiate chaincode and prime the ledger 
 docker-compose -f ./docker-compose.yml up -d cli
 
+# Only for one peer
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n dynamix -v 1.0 -p github.com/
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n dynamix -v 1.0 -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
 sleep 10
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n dynamix -c '{"function":"initLedger","Args":[""]}'
+
+# For every peer
+docker exec -e CORE_PEER_ADDRESS="peer0.org2.example.com:7051" -e "CORE_PEER_LOCALMSPID=Org2MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp" cli peer chaincode install -n dynamix -v 1.0 -p github.com/
 
 printf "\nTotal setup execution time : $(($(date +%s) - starttime)) secs ...\n\n\n"
 printf "Start by installing required packages run 'npm install'\n"
