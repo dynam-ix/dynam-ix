7a7084a90f19a8709222d579cbe489da07c76b08
Pedro de Botelho Marcos <pbmarcos@inf.ufrgs.br>
2018-01-04 15:50:17 -0200

    new start peer

--- distributed-network/start-peer.sh
+++ distributed-network/start-peer.sh
@@ -1,18 +1,19 @@
+#!/bin/sh
 
-CORE_PEER_ENDORSER_ENABLED=true \
-CORE_PEER_PROFILE_ENABLED=true \
-CORE_PEER_ADDRESS=peer0:7051 \
-CORE_PEER_CHAINCODELISTENADDRESS=peer0:7052 \
-CORE_PEER_ID=org0-peer0 \
-CORE_PEER_LOCALMSPID=Org0MSP \
-CORE_PEER_GOSSIP_EXTERNALENDPOINT=peer0:7051 \
-CORE_PEER_GOSSIP_USELEADERELECTION=true \
-CORE_PEER_GOSSIP_ORGLEADER=false \
-CORE_PEER_TLS_ENABLED=false \
-CORE_PEER_TLS_KEY_FILE=/home/hyperledger/dynam-ix/distributed-network/crypto-config/peerOrganizations/org0/peers/peer0.org0/tls/server.key \
-CORE_PEER_TLS_CERT_FILE=/home/hyperledger/dynam-ix/distributed-network/crypto-config/peerOrganizations/org0/peers/peer0.org0/tls/server.crt \
-CORE_PEER_TLS_ROOTCERT_FILE=/home/hyperledger/dynam-ix/distributed-network/crypto-config/peerOrganizations/org0/peers/peer0.org0/tls/ca.crt \
-CORE_PEER_TLS_SERVERHOSTOVERRIDE=peer0 \
-CORE_VM_DOCKER_ATTACHSTDOUT=true \
-CORE_PEER_MSPCONFIGPATH=/home/hyperledger/dynam-ix/distributed-network/crypto-config/peerOrganizations/org0/peers/peer0.org0/msp \
+export CORE_PEER_ENDORSER_ENABLED=true
+export CORE_PEER_PROFILE_ENABLED=true
+export CORE_PEER_ADDRESS=peer0:7051
+export CORE_PEER_CHAINCODELISTENADDRESS=peer0:7052
+export CORE_PEER_ID=org0-peer0
+export CORE_PEER_LOCALMSPID=Org0MSP
+export CORE_PEER_GOSSIP_EXTERNALENDPOINT=peer0:7051
+export CORE_PEER_GOSSIP_USELEADERELECTION=true
+export CORE_PEER_GOSSIP_ORGLEADER=false
+export CORE_PEER_TLS_ENABLED=false
+export CORE_PEER_TLS_KEY_FILE=/home/hyperledger/dynam-ix/distributed-network/crypto-config/peerOrganizations/org0/peers/peer0.org0/tls/server.key
+export CORE_PEER_TLS_CERT_FILE=/home/hyperledger/dynam-ix/distributed-network/crypto-config/peerOrganizations/org0/peers/peer0.org0/tls/server.crt
+export CORE_PEER_TLS_ROOTCERT_FILE=/home/hyperledger/dynam-ix/distributed-network/crypto-config/peerOrganizations/org0/peers/peer0.org0/tls/ca.crt
+export CORE_PEER_TLS_SERVERHOSTOVERRIDE=peer0
+export CORE_VM_DOCKER_ATTACHSTDOUT=true
+export CORE_PEER_MSPCONFIGPATH=/home/hyperledger/dynam-ix/distributed-network/crypto-config/peerOrganizations/org0/peers/peer0.org0/msp
 peer node start --peer-defaultchain=false
