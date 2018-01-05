7fa2300a6befc0b99ca54cd40756a6dcb52ad794
Pedro de Botelho Marcos <pbmarcos@inf.ufrgs.br>
2018-01-05 20:09:05 -0200

    new init scripts

---
+++ tmp/start-dist-peer.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Exit on first error, print all commands.
+set -ev
+
+# don't rewrite paths for Windows Git Bash users
+export MSYS_NO_PATHCONV=1
+
+export COMPOSE_PROJECT_NAME="net"
+
+docker-compose -f docker-compose-dist.yml down
+
+docker-compose -f docker-compose-dist.yml up -d cli couchdb-2 peer0.org2.example.com
+
+# wait for Hyperledger Fabric to start
+# incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
+export FABRIC_START_TIMEOUT=10
+#echo ${FABRIC_START_TIMEOUT}
+sleep ${FABRIC_START_TIMEOUT}
+
+# Join channel
+docker exec -e "CORE_PEER_LOCALMSPID=Org2MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org2.example.com/msp" peer0.org2.example.com peer channel join -b /etc/hyperledger/configtx/mychannel.block
+
+# Install chaincode
+docker exec -e CORE_PEER_ADDRESS="peer0.org2.example.com:7051" -e "CORE_PEER_LOCALMSPID=Org2MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp" cli peer chaincode install -n dynamix -v 1.0 -p github.com/
