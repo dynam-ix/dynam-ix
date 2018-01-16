c5003e9f65bd528f679ac070a01d14e10dcd0ac2
Pedro de Botelho Marcos <pbmarcos@inf.ufrgs.br>
2018-01-16 18:44:26 -0200

    blockchain size

--- evaluation/blockchainSize.bash
+++ evaluation/blockchainSize.bash
@@ -3,20 +3,20 @@
 export NUM_ASES=$1
 
 TRANSACTIONS=0
-SIZE=$(docker exec -it couchdb-1 du -h data/ | tail -n 1 | cut -f 1)
-echo $TRANSACTIONS $SIZE >> size.log
+SIZE=$(docker exec -it couchdb-1 du -b data/ | tail -n 1 | cut -f 1)
+echo $TRANSACTIONS $SIZE
 TRANSACTIONS=$((TRANSACTIONS+1))
 
 for (( AS=1; AS <= $NUM_ASES; AS++ )) do
     # Register N ASes
     docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n dynamix -c '{"function":"registerAS","Args":["AS'"${AS}"'", "200.132.227.120:50000", "Transit Provider", "0", "0", "asihd9asjd98asud0asid0asdsajd08as8hd98asj0djsa0djas"]}'
 
-    SIZE=$(docker exec -it couchdb-1 du -h data/ | tail -n 1 | cut -f 1)
-    echo $TRANSACTIONS $SIZE >> size.log
+    SIZE=$(docker exec -it couchdb-1 du -b data/ | tail -n 1 | cut -f 1)
+    echo $TRANSACTIONS $SIZE
     TRANSACTIONS=$((TRANSACTIONS+1))
 done
 
-for (( T=1; T <= 333333; T++ )) do
+for (( T=1; T <= 3000; T++ )) do
     for (( AS=1; AS <= $NUM_ASES; AS++ )) do
         # Generate agreement ID
         IA=$(date +%s%N |md5sum)
@@ -24,20 +24,20 @@ for (( T=1; T <= 333333; T++ )) do
         # Register Agreement
         docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n dynamix -c '{"function":"registerAgreement","Args":["IA-'"${IA}"'", "usahdiashmdiuuhasiudhasiudhasiuhdiusauhdiuashdiuashdiuasssaudhas", "'"${AS}"'", "'"${AS}"'", "uisaduihasiudhasiudhiuasuhdiuashdashdsaiudhiuashdas", "uisaduihasiudhasuhd12345678iuashdashdsaiudhiuashdas"]}'
 
-        SIZE=$(docker exec -it couchdb-1 du -h data/ | tail -n 1 | cut -f 1)
-        echo $TRANSACTIONS $SIZE >> size.log >> size.log
+        SIZE=$(docker exec -it couchdb-1 du -b data/ | tail -n 1 | cut -f 1)
+        echo $TRANSACTIONS $SIZE
         TRANSACTIONS=$((TRANSACTIONS+1))
 
         docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n dynamix -c '{"function":"updateCustRep","Args":["AS'"${AS}"'", "1"]}'
 
-        SIZE=$(docker exec -it couchdb-1 du -h data/ | tail -n 1 | cut -f 1)
-        echo $TRANSACTIONS $SIZE >> size.log
+        SIZE=$(docker exec -it couchdb-1 du -b data/ | tail -n 1 | cut -f 1)
+        echo $TRANSACTIONS $SIZE
         TRANSACTIONS=$((TRANSACTIONS+1))
 
         docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n dynamix -c '{"function":"updateProvRep","Args":["AS'"${AS}"'", "1"]}'
 
-        SIZE=$(docker exec -it couchdb-1 du -h data/ | tail -n 1 | cut -f 1)
-        echo $TRANSACTIONS $SIZE >> size.log
+        SIZE=$(docker exec -it couchdb-1 du -b data/ | tail -n 1 | cut -f 1)
+        echo $TRANSACTIONS $SIZE
         TRANSACTIONS=$((TRANSACTIONS+1))
     done
 done
--- evaluation/response.py
+++ evaluation/response.py
@@ -2,6 +2,7 @@
 import sys
 import os
 from datetime import datetime
+import numpy as np
 
 transactions = {}
 
@@ -10,7 +11,7 @@ def load(path):
     logs = os.listdir(path)
     # For all logs
     for log in logs:
-        if log.endswith(".log"):
+        if log.endswith(".log") and "AS" in log:
             l = open(log, "r")
             for line in l:
                 timestamp = line.split(";")[0]
@@ -27,20 +28,30 @@ def load(path):
 
             l.close()
 
+   # for key in sorted(transactions):
+   #     print key, transactions[key]
+
+
 def process():
  
-    for ID in transactions:
+    qt = []
+    et = []
+
+    for ID in sorted(transactions):
         queryTime = datetime.strptime(transactions[ID]["RO"], '%Y-%m-%d %H:%M:%S.%f') - datetime.strptime(transactions[ID]["SQ"], '%Y-%m-%d %H:%M:%S.%f')
+        qt.append(queryTime.seconds)
         queryProcess =  datetime.strptime(transactions[ID]["SO"], '%Y-%m-%d %H:%M:%S.%f') - datetime.strptime(transactions[ID]["RQ"], '%Y-%m-%d %H:%M:%S.%f')
         queryLatency = queryTime - queryProcess
         establishTime = datetime.strptime(transactions[ID]["RU"], '%Y-%m-%d %H:%M:%S.%f') - datetime.strptime(transactions[ID]["SP"], '%Y-%m-%d %H:%M:%S.%f')
+        et.append(establishTime.seconds)
         establishContract = datetime.strptime(transactions[ID]["SC"], '%Y-%m-%d %H:%M:%S.%f') - datetime.strptime(transactions[ID]["RP"], '%Y-%m-%d %H:%M:%S.%f')
         establishSign = datetime.strptime(transactions[ID]["SS"], '%Y-%m-%d %H:%M:%S.%f') - datetime.strptime(transactions[ID]["RC"], '%Y-%m-%d %H:%M:%S.%f')
         establishUpdate = datetime.strptime(transactions[ID]["SU"], '%Y-%m-%d %H:%M:%S.%f') - datetime.strptime(transactions[ID]["RS"], '%Y-%m-%d %H:%M:%S.%f')
         establishLatency = establishTime-establishContract-establishSign-establishUpdate
 
         print ID, queryTime, queryLatency, queryProcess, establishTime, establishContract, establishSign, establishUpdate, establishLatency
-
+    
+    print np.average(qt), np.average(et)
 
 #Main function
 if __name__ == "__main__":
