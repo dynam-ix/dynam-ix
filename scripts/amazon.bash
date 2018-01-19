c12ee530b97ce2029f3a052a8b6b6efd1b6e7ab2
Pedro de Botelho Marcos <pbmarcos@inf.ufrgs.br>
2018-01-19 18:35:29 -0200

    amazon script

--- scripts/amazon.bash
+++ scripts/amazon.bash
@@ -12,9 +12,9 @@ AS=2
 while IFS='' read -r line || [[ -n "$line" ]]; do
 
     echo $line
-    rsh -i "${KEY_PATH}" ubuntu@${line} "echo $line ; ls " &
+    #rsh -i "${KEY_PATH}" ubuntu@${line} "echo $line ; ls " &
 
-    #rsh -i "dynamix.pem" ubuntu@${HOST} "cd dynam-ix/experiments/${EXP_NUM}-experiment ; ./initPeer.bash ${AS} Transit intents.json ${ORDERER_IP} autonomous ${REQUESTS} ${SLEEP}"
+    rsh -i "${KEY_PATH}" ubuntu@${line} "cd dynam-ix/experiments/${EXP_NUM}-experiment ; ./initPeer.bash ${AS} Transit intents.json ${ORDERER_IP} autonomous ${REQUESTS} ${SLEEP} > run.log" &
 
     AS=$((AS+1))
 
