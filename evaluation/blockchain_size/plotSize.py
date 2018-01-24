c5e62e55f8920feac47dabbe307927fafc827e73
Pedro de Botelho Marcos <pbmarcos@inf.ufrgs.br>
2018-01-24 21:00:07 -0200

    general updates'

--- evaluation/blockchain_size/plotSize.py
+++ evaluation/blockchain_size/plotSize.py
@@ -1,42 +1,51 @@
 #!/usr/bin/python
 import sys
 import os
-import matplotlib
+import matplotlib.pyplot as plt
 import pylab
 import numpy as np
 
-def plot(file):
+def plot(path):
 
-    size = open(file, "r")
-    x = []
-    y = []
+    plt.figure()
+    plt.yscale('log')
 
-    for line in size:
-        t = line.split(" ")[0]
-        s = line.split(" ")[1]
+    files = os.listdir(path)
+    for file in files:
+        if ".log" in file:
 
-        y.append(float(s)/(1024*1024))
+            size = open(path+file, "r")
+            x = []
+            y = []
 
-        x.append(int(t))
+            for line in size:
+                t = line.split(" ")[0]
+                s = line.split(" ")[1]
 
-    size.close()
+                y.append(float(s)/(1024*1024))
 
-    print len(x), len(y)
+                x.append(int(t))
 
-    #pylab.title("Blockchain size x cumulative number of transactions", fontsize=18)
-    pylab.plot(x, y, linestyle="solid", linewidth=2)
-    pylab.ylabel("Blockchain size (MB)", fontsize=18)
-    pylab.xlabel("Cumulative number of transactions", fontsize=18)
-    pylab.grid(True)
-    pylab.xlim(0, 10000)
-    pylab.ylim(0, max(y))
-    #pylab.legend(loc="best", fontsize=14)
-    pylab.savefig('blockchainSize.pdf', dpi=600)
-    pylab.savefig('blockchainSize.png', dpi=600)
-    pylab.show() #uncomment to show plots during execution
-    pylab.clf()
+            size.close()
+
+            print len(x), len(y)
+
+            #pylab.title("Blockchain size x cumulative number of transactions", fontsize=18)
+            plt.plot(x, y, linestyle="solid", linewidth=2, label=file.split(".log")[0])
+
+
+    plt.ylabel("Blockchain size (MB)", fontsize=18)
+    plt.xlabel("Cumulative number of transactions", fontsize=18)
+    plt.grid(True)
+    plt.xlim(0, 10000)
+    plt.ylim(0, )
+    plt.legend(loc="best", ncol=2)
+    plt.savefig('blockchainSize.pdf', dpi=600)
+    plt.savefig('blockchainSize.png', dpi=600)
+    plt.show() #uncomment to show plots during execution
+    plt.clf()
 
 #Main function
 if __name__ == "__main__":
-    file = sys.argv[1]
-    plot(file)
+    path = sys.argv[1]
+    plot(path)
