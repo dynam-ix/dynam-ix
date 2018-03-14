486f6cdc185a506bd30a704c1a034628d6944b69
Pedro de Botelho Marcos <pbmarcos@inf.ufrgs.br>
2018-03-14 20:47:00 -0300

    Fixing paths on run.bash (blockchainSize) and creating readme.md files

--- evaluation/blockchainSize/run.bash
+++ evaluation/blockchainSize/run.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export AS=1
-export ORDERER_IP="x.x.x.x" # set the proper IP
+export ORDERER_IP="127.0.0.1" # set the proper IP
 export COMPOSE_PROJECT_NAME="net"
 
 # Cleaning any previous experiment
@@ -9,17 +9,17 @@ echo "Cleaning docker environment"
 docker rm -f $(docker ps -aq)
 docker rmi $(docker images dev-* -q)
 docker network prune -f
-docker-compose -f ../../experiments/1-experiment-1tpb-1s/docker-compose-base.yml down
+docker-compose -f 1-AS-1-tpb-1s-timeout/docker-compose-base.yml down
 # Start Docker Containers
 echo "Staring docker containers"
-docker-compose -f ../../experiments/1-experiment-1tpb-1s/docker-compose-base.yml up -d peer ca couchdb cli orderer
+docker-compose -f 1-AS-1-tpb-1s-timeout/docker-compose-base.yml up -d peer ca couchdb cli orderer
 
 # Create channel
 echo "Creating channel"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org${AS}.example.com/msp" peer0.org${AS}.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
 echo "Copying block"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org${AS}.example.com/msp" peer0.org${AS}.example.com cp mychannel.block /etc/hyperledger/configtx/
-sleep 15
+sleep 10
 # Join channel
 echo "Joining channel"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org${AS}.example.com/msp" peer0.org${AS}.example.com peer channel join -b /etc/hyperledger/configtx/mychannel.block
@@ -30,7 +30,7 @@ docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/o
 echo "Instantiating chaincode"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org${AS}.example.com/users/Admin@org${AS}.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n dynamix -v 1.0 -c '{"Args":[""]}'
 # Init (Invoke) chaincode
-sleep 62
+sleep 10
 echo "Invoking chaincode"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org${AS}.example.com/users/Admin@org${AS}.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n dynamix -c '{"function":"initLedger","Args":[""]}'
 
@@ -43,17 +43,17 @@ echo "Cleaning docker environment"
 docker rm -f $(docker ps -aq)
 docker rmi $(docker images dev-* -q)
 docker network prune -f
-docker-compose -f ../../experiments/1-experiment-4tpb-1s/docker-compose-base.yml down
+docker-compose -f 1-AS-4-tpb-1s-timeout/docker-compose-base.yml down
 # Start Docker Containers
 echo "Staring docker containers"
-docker-compose -f ../../experiments/1-experiment-4tpb-1s/docker-compose-base.yml up -d peer ca couchdb cli orderer
+docker-compose -f 1-AS-4-tpb-1s-timeout/docker-compose-base.yml up -d peer ca couchdb cli orderer
 
 # Create channel
 echo "Creating channel"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org${AS}.example.com/msp" peer0.org${AS}.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
 echo "Copying block"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org${AS}.example.com/msp" peer0.org${AS}.example.com cp mychannel.block /etc/hyperledger/configtx/
-sleep 15
+sleep 10
 # Join channel
 echo "Joining channel"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org${AS}.example.com/msp" peer0.org${AS}.example.com peer channel join -b /etc/hyperledger/configtx/mychannel.block
@@ -64,7 +64,7 @@ docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/o
 echo "Instantiating chaincode"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org${AS}.example.com/users/Admin@org${AS}.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n dynamix -v 1.0 -c '{"Args":[""]}'
 # Init (Invoke) chaincode
-sleep 62
+sleep 10
 echo "Invoking chaincode"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org${AS}.example.com/users/Admin@org${AS}.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n dynamix -c '{"function":"initLedger","Args":[""]}'
 
@@ -75,17 +75,17 @@ echo "Cleaning docker environment"
 docker rm -f $(docker ps -aq)
 docker rmi $(docker images dev-* -q)
 docker network prune -f
-docker-compose -f ../../experiments/1-experiment-15tpb-15s/docker-compose-base.yml down
+docker-compose -f 1-AS-15-tpb-15s-timeout/docker-compose-base.yml down
 # Start Docker Containers
 echo "Staring docker containers"
-docker-compose -f ../../experiments/1-experiment-15tpb-15s/docker-compose-base.yml up -d peer ca couchdb cli orderer
+docker-compose -f 1-AS-15-tpb-15s-timeout/docker-compose-base.yml up -d peer ca couchdb cli orderer
 
 # Create channel
 echo "Creating channel"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org${AS}.example.com/msp" peer0.org${AS}.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
 echo "Copying block"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org${AS}.example.com/msp" peer0.org${AS}.example.com cp mychannel.block /etc/hyperledger/configtx/
-sleep 15
+sleep 10
 # Join channel
 echo "Joining channel"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org${AS}.example.com/msp" peer0.org${AS}.example.com peer channel join -b /etc/hyperledger/configtx/mychannel.block
@@ -96,7 +96,7 @@ docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/o
 echo "Instantiating chaincode"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org${AS}.example.com/users/Admin@org${AS}.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n dynamix -v 1.0 -c '{"Args":[""]}'
 # Init (Invoke) chaincode
-sleep 62
+sleep 20
 echo "Invoking chaincode"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org${AS}.example.com/users/Admin@org${AS}.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n dynamix -c '{"function":"initLedger","Args":[""]}'
 
@@ -109,17 +109,17 @@ echo "Cleaning docker environment"
 docker rm -f $(docker ps -aq)
 docker rmi $(docker images dev-* -q)
 docker network prune -f
-docker-compose -f ../../experiments/1-experiment-30tpb-30s/docker-compose-base.yml down
+docker-compose -f 1-AS-30-tpb-30s-timeout/docker-compose-base.yml down
 # Start Docker Containers
 echo "Staring docker containers"
-docker-compose -f ../../experiments/1-experiment-30tpb-30s/docker-compose-base.yml up -d peer ca couchdb cli orderer
+docker-compose -f 1-AS-30-tpb-30s-timeout/docker-compose-base.yml up -d peer ca couchdb cli orderer
 
 # Create channel
 echo "Creating channel"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org${AS}.example.com/msp" peer0.org${AS}.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
 echo "Copying block"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org${AS}.example.com/msp" peer0.org${AS}.example.com cp mychannel.block /etc/hyperledger/configtx/
-sleep 15
+sleep 10
 # Join channel
 echo "Joining channel"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org${AS}.example.com/msp" peer0.org${AS}.example.com peer channel join -b /etc/hyperledger/configtx/mychannel.block
@@ -130,7 +130,7 @@ docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/o
 echo "Instantiating chaincode"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org${AS}.example.com/users/Admin@org${AS}.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n dynamix -v 1.0 -c '{"Args":[""]}'
 # Init (Invoke) chaincode
-sleep 62
+sleep 35
 echo "Invoking chaincode"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org${AS}.example.com/users/Admin@org${AS}.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n dynamix -c '{"function":"initLedger","Args":[""]}'
 
@@ -143,17 +143,17 @@ echo "Cleaning docker environment"
 docker rm -f $(docker ps -aq)
 docker rmi $(docker images dev-* -q)
 docker network prune -f
-docker-compose -f ../../experiments/1-experiment-60tpb-60s/docker-compose-base.yml down
+docker-compose -f 1-AS-60-tpb-60s-timeout/docker-compose-base.yml down
 # Start Docker Containers
 echo "Staring docker containers"
-docker-compose -f ../../experiments/1-experiment-60tpb-60s/docker-compose-base.yml up -d peer ca couchdb cli orderer
+docker-compose -f 1-AS-60-tpb-60s-timeout/docker-compose-base.yml up -d peer ca couchdb cli orderer
 
 # Create channel
 echo "Creating channel"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org${AS}.example.com/msp" peer0.org${AS}.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
 echo "Copying block"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org${AS}.example.com/msp" peer0.org${AS}.example.com cp mychannel.block /etc/hyperledger/configtx/
-sleep 15
+sleep 10
 # Join channel
 echo "Joining channel"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org${AS}.example.com/msp" peer0.org${AS}.example.com peer channel join -b /etc/hyperledger/configtx/mychannel.block
@@ -164,7 +164,7 @@ docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/o
 echo "Instantiating chaincode"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org${AS}.example.com/users/Admin@org${AS}.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n dynamix -v 1.0 -c '{"Args":[""]}'
 # Init (Invoke) chaincode
-sleep 62
+sleep 65
 echo "Invoking chaincode"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org${AS}.example.com/users/Admin@org${AS}.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n dynamix -c '{"function":"initLedger","Args":[""]}'
 
@@ -176,17 +176,17 @@ echo "Cleaning docker environment"
 docker rm -f $(docker ps -aq)
 docker rmi $(docker images dev-* -q)
 docker network prune -f
-docker-compose -f ../../experiments/1-experiment-60tpb-15s/docker-compose-base.yml down
+docker-compose -f 1-AS-60-tpb-15s-timeout/docker-compose-base.yml down
 # Start Docker Containers
 echo "Staring docker containers"
-docker-compose -f ../../experiments/1-experiment-60tpb-15s/docker-compose-base.yml up -d peer ca couchdb cli orderer
+docker-compose -f 1-AS-60-tpb-15s-timeout/docker-compose-base.yml up -d peer ca couchdb cli orderer
 
 # Create channel
 echo "Creating channel"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org${AS}.example.com/msp" peer0.org${AS}.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
 echo "Copying block"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org${AS}.example.com/msp" peer0.org${AS}.example.com cp mychannel.block /etc/hyperledger/configtx/
-sleep 15
+sleep 10
 # Join channel
 echo "Joining channel"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org${AS}.example.com/msp" peer0.org${AS}.example.com peer channel join -b /etc/hyperledger/configtx/mychannel.block
@@ -197,7 +197,7 @@ docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/o
 echo "Instantiating chaincode"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org${AS}.example.com/users/Admin@org${AS}.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n dynamix -v 1.0 -c '{"Args":[""]}'
 # Init (Invoke) chaincode
-sleep 62
+sleep 20
 echo "Invoking chaincode"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org${AS}.example.com/users/Admin@org${AS}.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n dynamix -c '{"function":"initLedger","Args":[""]}'
 
@@ -210,17 +210,17 @@ echo "Cleaning docker environment"
 docker rm -f $(docker ps -aq)
 docker rmi $(docker images dev-* -q)
 docker network prune -f
-docker-compose -f ../../experiments/1-experiment-120tpb-30s/docker-compose-base.yml down
+docker-compose -f 1-AS-120-tpb-30s-timeout/docker-compose-base.yml down
 # Start Docker Containers
 echo "Staring docker containers"
-docker-compose -f ../../experiments/1-experiment-120tpb-30s/docker-compose-base.yml up -d peer ca couchdb cli orderer
+docker-compose -f 1-AS-120-tpb-30s-timeout/docker-compose-base.yml up -d peer ca couchdb cli orderer
 
 # Create channel
 echo "Creating channel"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org${AS}.example.com/msp" peer0.org${AS}.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
 echo "Copying block"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org${AS}.example.com/msp" peer0.org${AS}.example.com cp mychannel.block /etc/hyperledger/configtx/
-sleep 15
+sleep 10
 # Join channel
 echo "Joining channel"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org${AS}.example.com/msp" peer0.org${AS}.example.com peer channel join -b /etc/hyperledger/configtx/mychannel.block
@@ -231,7 +231,7 @@ docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/o
 echo "Instantiating chaincode"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org${AS}.example.com/users/Admin@org${AS}.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n dynamix -v 1.0 -c '{"Args":[""]}'
 # Init (Invoke) chaincode
-sleep 62
+sleep 35
 echo "Invoking chaincode"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org${AS}.example.com/users/Admin@org${AS}.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n dynamix -c '{"function":"initLedger","Args":[""]}'
 
@@ -244,17 +244,17 @@ echo "Cleaning docker environment"
 docker rm -f $(docker ps -aq)
 docker rmi $(docker images dev-* -q)
 docker network prune -f
-docker-compose -f ../../experiments/1-experiment-240tpb-60s/docker-compose-base.yml down
+docker-compose -f 1-AS-240-tpb-60s-timeout/docker-compose-base.yml down
 # Start Docker Containers
 echo "Staring docker containers"
-docker-compose -f ../../experiments/1-experiment-240tpb-60s/docker-compose-base.yml up -d peer ca couchdb cli orderer
+docker-compose -f 1-AS-240-tpb-6s-timeout/docker-compose-base.yml up -d peer ca couchdb cli orderer
 
 # Create channel
 echo "Creating channel"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org${AS}.example.com/msp" peer0.org${AS}.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
 echo "Copying block"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org${AS}.example.com/msp" peer0.org${AS}.example.com cp mychannel.block /etc/hyperledger/configtx/
-sleep 15
+sleep 10
 # Join channel
 echo "Joining channel"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org${AS}.example.com/msp" peer0.org${AS}.example.com peer channel join -b /etc/hyperledger/configtx/mychannel.block
@@ -265,7 +265,7 @@ docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/o
 echo "Instantiating chaincode"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org${AS}.example.com/users/Admin@org${AS}.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n dynamix -v 1.0 -c '{"Args":[""]}'
 # Init (Invoke) chaincode
-sleep 62
+sleep 65
 echo "Invoking chaincode"
 docker exec -e "CORE_PEER_LOCALMSPID=Org${AS}MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org${AS}.example.com/users/Admin@org${AS}.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n dynamix -c '{"function":"initLedger","Args":[""]}'
 
