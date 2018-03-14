d01ee6d29108eb91836b51be526afc1fa0e3c67a
Pedro de Botelho Marcos <pbmarcos@inf.ufrgs.br>
2018-03-14 15:01:38 -0300

    tools path

--- tools/experimentGenerator/generateExperiment.bash
+++ tools/experimentGenerator/generateExperiment.bash
@@ -21,40 +21,40 @@ export FABRIC_CFG_PATH=$PWD
 
 # Generate crypto-config.yaml
 echo "Generating cryto-config.yaml file"
-python ${DYNAMIX_DIR}/scripts/experimentGenerator/generateCryptoConfig.py $NUM_ORGS > crypto-config.yaml
+python ${DYNAMIX_DIR}/tools/experimentGenerator/generateCryptoConfig.py $NUM_ORGS > crypto-config.yaml
 # Generate configtx.yaml
 echo "Generating configtx.yaml file"
-python ${DYNAMIX_DIR}/scripts/experimentGenerator/generateConfigtx.py $NUM_ORGS > configtx.yaml
+python ${DYNAMIX_DIR}/tools/experimentGenerator/generateConfigtx.py $NUM_ORGS > configtx.yaml
 
 # Generate certifactes
 echo "Generating certificates"
-$DYNAMIX_DIR/tools/cryptogen generate --config=./crypto-config.yaml 
+$DYNAMIX_DIR/tools/HLFConfig/cryptogen generate --config=./crypto-config.yaml 
 
 echo "Generating channel artifacts"
 mkdir -p config
 # Genesis block
 echo "Generating genesis block"
-$DYNAMIX_DIR/tools/configtxgen -profile SingleOrgOrdererGenesis -outputBlock ./config/genesis.block
+$DYNAMIX_DIR/tools/HLFConfig/configtxgen -profile SingleOrgOrdererGenesis -outputBlock ./config/genesis.block
 # Channnel
 echo "Creating channel"
-$DYNAMIX_DIR/tools/configtxgen -profile MultipleOrgChannel -outputCreateChannelTx ./config/channel.tx -channelID mychannel
+$DYNAMIX_DIR/tools/HLFConfig/configtxgen -profile MultipleOrgChannel -outputCreateChannelTx ./config/channel.tx -channelID mychannel
 # Anchor peer update for each org
 for (( ASN=1; ASN <= $NUM_ORGS; ASN++ )) do
     echo "Updating anchor peer for org $ASN" # TODO Repeat for all peers
-    $DYNAMIX_DIR/tools/configtxgen -profile MultipleOrgChannel -outputAnchorPeersUpdate ./config/Org${ASN}MSPanchors.tx -channelID mychannel -asOrg Org${ASN}MSP
+    $DYNAMIX_DIR/tools/HLFConfig/configtxgen -profile MultipleOrgChannel -outputAnchorPeersUpdate ./config/Org${ASN}MSPanchors.tx -channelID mychannel -asOrg Org${ASN}MSP
 done
 
 # Generate ca-file
 echo "Generating CA config file"
 mkdir -p ca-server-config
-python ${DYNAMIX_DIR}/scripts/experimentGenerator/generateCAConfig.py $NUM_ORGS > ca-server-config/fabric-ca-server-config.yaml
+python ${DYNAMIX_DIR}/tools/experimentGenerator/generateCAConfig.py $NUM_ORGS > ca-server-config/fabric-ca-server-config.yaml
 
 # Copy base files
-echo "Copying experiment scripts"
-cp ${DYNAMIX_DIR}/scripts/experimentGenerator/base/docker-compose-base.yml ${DYNAMIX_DIR}/experiments/${EXPERIMENT}
-cp ${DYNAMIX_DIR}/scripts/experimentGenerator/base/initExperiment.bash ${DYNAMIX_DIR}/experiments/${EXPERIMENT}
-cp ${DYNAMIX_DIR}/scripts/experimentGenerator/base/initPeer.bash ${DYNAMIX_DIR}/experiments/${EXPERIMENT}
-cp ${DYNAMIX_DIR}/scripts/experimentGenerator/base/initOrderer.bash ${DYNAMIX_DIR}/experiments/${EXPERIMENT}
+echo "Copying experiment base files"
+cp ${DYNAMIX_DIR}/tools/experimentGenerator/base/docker-compose-base.yml ${DYNAMIX_DIR}/experiments/${EXPERIMENT}
+cp ${DYNAMIX_DIR}/tools/experimentGenerator/base/initExperiment.bash ${DYNAMIX_DIR}/experiments/${EXPERIMENT}
+cp ${DYNAMIX_DIR}/tools/experimentGenerator/base/initPeer.bash ${DYNAMIX_DIR}/experiments/${EXPERIMENT}
+cp ${DYNAMIX_DIR}/tools/experimentGenerator/base/initOrderer.bash ${DYNAMIX_DIR}/experiments/${EXPERIMENT}
 
 # Print instructions
 echo "Configuration files generated successfully!!!"
